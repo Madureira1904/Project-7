0d5ee994b6ca6e23009a81d50daa1ca4f1ef29f8
Madureira1904 git config --global user.email miguelouteirooliveira@gmail.com <miguelouteirooliveira@MacBookAir.lan>
2026-02-16 13:39:31 +0100

    Ajout du code JS pour la funcionnalité pour ajouter des works à galerie(fetch: POST) avec preview des images et CSS correspondant

--- FrontEnd/assets/JS/script.js
+++ FrontEnd/assets/JS/script.js
@@ -1,4 +1,4 @@
-console.log("JavaScript front-end ligado"); // testing connection
+console.log("JavaScript front-end on"); // testing connection
 
 //im selecting all the filtter-buttons
 const filterButtons = document.querySelector(".filter-buttons");
@@ -105,6 +105,8 @@ function filterWorks(category, works) {
   }
 }
 
+
+
 // --- MAIN FUNCTION --- //
 
 async function init() {
@@ -156,9 +158,37 @@ async function init() {
   const addPhotoButton = document.querySelector(".add-photo-button");
   const backToGallery = document.querySelector(".back-to-gallery");
 
-  const addProjectForm = document.getElementById("add-project-form");
+  const addProjectForm = document.getElementById("add-project-form"); 
   const categorySelect = document.getElementById("category");
 
+// image preview
+  const imageInput = document.getElementById("image");
+  const imagePreview = document.getElementById("image-preview");
+
+
+  // preview of the photo in the form
+  imageInput.addEventListener("change", () => {
+
+  const file = imageInput.files[0];
+
+  if (!file) return;
+
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+
+    imagePreview.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = e.target.result;
+    img.alt = "Preview";
+
+    imagePreview.appendChild(img);
+  };
+
+  reader.readAsDataURL(file);
+
+});
 
 
   // display works inside of modale
@@ -181,7 +211,7 @@ async function init() {
       deleteIcon.addEventListener("click", async () => {
         await deleteWork(work.id); // calls the function to do delete on the a função que faz DELETE na API
 
-        // Refresh works
+        // refresh works
         const updatedWorks = await getWorks();
         displayWorks(updatedWorks);
         displayWorksInModal(updatedWorks);
@@ -202,6 +232,55 @@ async function init() {
     categorySelect.appendChild(option);
   });
 
+  // add project 
+  addProjectForm.addEventListener("submit", async (event) => {
+
+  event.preventDefault(); // prevents the page of refreshing
+
+  // selects the elements of the form
+  const imageInput = document.getElementById("image");
+  const titleInput = document.getElementById("title");
+  const categoryInput = document.getElementById("category");
+
+  const image = imageInput.files[0];
+  const title = titleInput.value;
+  const category = categoryInput.value;
+
+  // validation: verifies if all camps all filled
+  if (!image || !title || !category) {
+    alert("Veuillez remplir tous les champs");
+    return;
+  }
+
+  // cria um objeto FormData para enviar os dados como multipart/form-data
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("title", title);
+  formData.append("category", category);
+
+  // sends the data for the API using fetch
+  await fetch("http://localhost:5678/api/works", {
+    method: "POST", // método POST to create a new project
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    body: formData
+  });
+
+  // refresh gallery dynamically
+  const updatedWorks = await getWorks();
+
+  displayWorks(updatedWorks); // refresh the gallery in the main page atualiza
+  displayWorksInModal(updatedWorks); // refresh the gallery in the modale
+
+  // reset form
+  addProjectForm.reset();
+
+  // go back to gallery page
+  formView.style.display = "none";
+  galleryView.style.display = "block"; // goes back to gallery modale
+
+});
 
 
   // open modal
